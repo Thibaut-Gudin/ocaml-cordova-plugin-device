bbdca9ec07f0f7a8bddbdb9d67bd29d1447ebd13
Danny Willems <contact@danny-willems.be>
2016-03-08 11:32:21 +0100

    Manufacter --> manufacturer. Remove the creation  of a variable called 'device' because the deviceready event must be handled before

--- device.ml
+++ device.ml
@@ -1,10 +1,13 @@
 class type device =
   object
     method available : bool Js.t Js.readonly_prop
+
     (* smartphone platform *)
     method platform : Js.js_string Js.t Js.readonly_prop
+
     (* OS version *)
     method version : Js.js_string Js.t Js.readonly_prop
+
     (* smartphone uuid *)
     method uuid: Js.js_string Js.t Js.readonly_prop
 
@@ -17,11 +20,9 @@ class type device =
     (* If the application's running on a phone *)
     method isVirtual : bool Js.t Js.readonly_prop
 
-    (* Manufacter *)
-    method manufacter : Js.js_string Js.t Js.readonly_prop
+    (* Manufacturer *)
+    method manufacturer : Js.js_string Js.t Js.readonly_prop
 
     (* Serial *)
     method serial : Js.js_string Js.t Js.readonly_prop
   end
-
-let device : device Js.t = Js.Unsafe.js_expr ("device")
